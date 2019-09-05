8445e901f31d281d2ce76fbacf48c22e8bb82d53
Vicente Brida <vicentebrida@outlook.com>
2019-09-04 23:30:12 -0300

    Create redux example

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import TodoList from './TodoList';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <TodoList />
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
